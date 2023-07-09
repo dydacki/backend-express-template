e83d24f279fce3d9ba3049a29e6b42d00315cb04
dydacki <dydacki.wojciech@gmail.com>
2023-07-09 19:17:41 +0200

    refactor: add server-related dependencies. Implement first version of express server

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+
+const app = express();
+app
+  .use(cookieParser())
+  .use(bodyParser.json())
+  .use(compression())
+  .use(
+    cors({
+      credentials: true,
+    }),
+  );
+
+const server = http.createServer(app);
+
+server.listen(8080, () => console.log('Server running on http://localhost:8080'));
