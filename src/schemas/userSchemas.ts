4d5608da7a810515074e8434f2f9e8e6f74cff71
dydacki <dydacki.wojciech@gmail.com>
2023-07-23 12:17:59 +0200

    refactor: minor refactoring

--- src/schemas/userSchemas.ts
+++ src/schemas/userSchemas.ts
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 const createUserSchema = Joi.object({
   email: Joi.string().email().required(),
-  userName: Joi.string().required(),
+  userName: Joi.string().required().max(24),
   password: Joi.string().required().min(9),
   passwordConfirmation: Joi.string().required().valid(Joi.ref('password')).messages({ 'any.only': 'password confirmation must match the password' }),
 });
