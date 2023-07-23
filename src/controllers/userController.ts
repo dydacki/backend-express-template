268aefe282fbeae355457e09fc6b299e4049d272
dydacki <dydacki.wojciech@gmail.com>
2023-07-23 12:14:35 +0200

    refactor: hashing user password with argon2

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,15 +1,17 @@
+import argon2 from 'argon2';
 import { Request, Response } from 'express';
 import { Types } from 'mongoose';
 import User from '../models/User';
 
 const createUser = async (request: Request, response: Response) => {
   const { email, userName, password } = request.body;
+  const hashedPassword = await argon2.hash(password);
 
   const user = new User({
     _id: new Types.ObjectId(),
     email,
     userName,
-    password,
+    password: hashedPassword,
   });
 
   return user
